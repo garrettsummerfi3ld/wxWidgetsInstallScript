e53990e28e055e1e3171bcd3d1c724482ec15560
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2022-12-01 01:26:47 -0600

    Finish script
    This script can now download, extract, build, and set environment variables,
    
    Linted whole document with autopep8.
    
    Added README with instructions to use script.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # wxWidgetsInstallScript
+
+A small Python script to install wxWidgets 3.2.1 and build automatically.
+
+Tested on Windows 10/11 with Visual Studio 2022 (17)
+
+## Requirements
+
+- Visual Studio with Desktop C++ installed
+- Python 3
+- Internet connection
+
+## What does this script do?
+
+This script downloads a specific release of wxWidgets and builds from that source and sets environment variables on the whole system.
+
+Current configurations and architectures built are:
+
+- Debug x64
+- Debug Win32
+- Release x64
+- Release Win32
+
+## Steps to run
+
+- Clone or download `setup.py`
+- Open an **administrative** Visual Studio PowerShell or Command Prompt
+  - You may need to go to the Start Menu > Visual Studio (`Version`) > Developer ... > Right-click and Run as Administrator
+- `cd` to your downloaded `setup.py` file
+- `python .\setup.py`
--- setup.py
+++ setup.py
@@ -1,10 +1,73 @@
+import ctypes
 import urllib.request
 import os
+import shutil
+
+# Variables
+url = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.1/wxWidgets-3.2.1.zip"
+filename = "wxWidgets-3.2.1.zip"
+finalpath = "C:\\wxWidgets-3.2.1"
+
 
 def main():
-    url = "https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.1/wxWidgets-3.2.1.zip"
-    filename = "wxWidgets-3.2.1.zip"
+    # Main
+    downloadWxWidgets()
+    extractWxWidgets()
+    buildWxWidgets()
+    setEnvironmentVariables()
+
+
+def downloadWxWidgets():
+    # Download wxWidgets
+
+    print("[-] Downloading wxWidgets...")
     urllib.request.urlretrieve(url, filename)
+    print("[-] Download complete!")
+
+
+def extractWxWidgets():
+    # Extract wxWidgets
+    print("[-] Extracting wxWidgets...")
+    shutil.unpack_archive(filename, finalpath)
+    shutil.move(os.getcwd() + f"\\{filename}", f"{finalpath}.zip")
+    print("[-] Extraction complete!")
+
+
+def buildWxWidgets():
+    # Build wxWidgets
+    # This is a bit of a hack, but it works
+    print("[-] Building wxWidgets...")
+    os.chdir(f"{finalpath}\\build\\msw")
+    print("[-] Building wxWidgets Debug (x64)...")
+    os.system("msbuild wx_vc17.sln /p:Configuration=Debug /p:Platform=x64")
+    print("[-] Building wxWidgets Release (x64)...")
+    os.system("msbuild wx_vc17.sln /p:Configuration=Release /p:Platform=x64")
+    print("[-] Building wxWidgets Debug (Win32)...")
+    os.system("msbuild wx_vc17.sln /p:Configuration=Debug /p:Platform=Win32")
+    print("[-] Building wxWidgets Release (Win32)...")
+    os.system("msbuild wx_vc17.sln /p:Configuration=Release /p:Platform=Win32")
+    print("[-] Build complete!")
+
+
+def setEnvironmentVariables():
+    # Set environment variables
+    print("[-] Setting environment variables...")
+    os.system(f"setx WXWIN \"{finalpath}\" /M")
+    print("[-] Environment variables set!")
+
+
+def checkAdmin():
+    # Check if running as admin
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except Exception:
+        return False
+
 
 if __name__ == "__main__":
-    main()
+    print("[-] Checking for admin privileges...")
+    if checkAdmin():
+        main()
+    else:
+        print("[!] Please run as admin")
+        exit(0)
